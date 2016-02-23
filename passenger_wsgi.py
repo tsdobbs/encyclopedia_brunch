844f659d3976b5173ee552dfba13aacd5ede551e
tsdobbs <tim.dobbs@gmail.com>
2016-02-23 15:47:38 -0800

    Removed a file that didn't need to be tracked

--- passenger_wsgi.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-virt_binary = "/home/timdobbs/.env/env/34/bin/python"
-if sys.executable != virt_binary: os.execl(virt_binary, virt_binary, *sys.argv)
-sys.path.append(os.getcwd())
-from __init__ import app as application
