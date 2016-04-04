812521426970dec4f66ba411e88e878339e38ac2
tsdobbs <tim.dobbs@gmail.com>
2016-04-03 22:32:58 -0700

    Accounted for static images and audio possibly needing to be saved in different folders on different machines

--- config.py
+++ config.py
@@ -11,7 +11,16 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'my_secret'
 
-AUDIO_UPLOAD_FOLDER = os.path.join(basedir,'static','audio')
+#This accounts for if the static images and audio need to be saved somewhere besides the directory where the code is stored.
+#It checks the parent directory of the code directory for a folder that matches alternate_path. If it exists, it uses
+#it. Otherwise it uses the basedir
+alternate_path = "public_html"
+if os.path.exists(os.path.exists(os.path.join(os.path.split(os.path.abspath(os.path.dirname(__file__)))[0],alternate_path))):
+    static_basedir = os.path.join(os.path.split(os.path.abspath(os.path.dirname(__file__)))[0],alternate_path)
+else:
+    static_basedir = basedir
+
+AUDIO_UPLOAD_FOLDER = os.path.join(static_basedir,'static','audio')
 ALLOWED_AUDIO_EXTENSIONS = set(['mp3','mp4','aac','wav','flac'])
-IMAGE_UPLOAD_FOLDER = os.path.join(basedir,'static','img','post_images')
+IMAGE_UPLOAD_FOLDER = os.path.join(static_basedir,'static','img','post_images')
 ALLOWED_IMAGE_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'svg'])
